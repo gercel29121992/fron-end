692935a72e5d7a4921fc6cc4e78833bbb730c78a
gercelluciano <gercelluciano@hotmail.com>
2023-07-11 21:13:03 -0400

    registrar user

--- src/app/seguridad/useradmin/herramientas/list-user-app/list-user-app.component.ts
+++ src/app/seguridad/useradmin/herramientas/list-user-app/list-user-app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { paginacion } from 'src/app/seguridad/seguridad';
 import { SeguridadService } from 'src/app/seguridad/seguridad.service';
 
@@ -9,18 +10,32 @@ import { SeguridadService } from 'src/app/seguridad/seguridad.service';
   styleUrls: ['./list-user-app.component.css']
 })
 export class ListUserAppComponent implements OnInit {
+  listFiltered: any;
+  displayedColumns: string[] = [ 'Email', 'Nombre', 'Apellido','Estado','Bloquear'];
+  
 
   constructor(private seguridadService: SeguridadService,
     private router: Router) { }
     paginacion:paginacion={paginas:1,record:1};
+
 user:any
+searchTerm$ = new Subject<string>();
   ngOnInit(): void {
     this.seguridadService.obtenerUsuarios(1,10)
     .subscribe(users =>{this.user=users.body;
+      this.listFiltered = this.user;
       console.log(this.user);}
     ,error=> console.error(error));
+    
+   
+    this.filterList();
    
   }
-
+  filterList(): void {
+    this.searchTerm$.subscribe(term => {
+      this.listFiltered = this.user
+        .filter(item => item.email.toLowerCase().indexOf(term.toLowerCase()) >= 0);
+    });
+  }
 
 }
